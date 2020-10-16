ece2d531e003746b974af9d5d2856812832cd6e3
Lars Larsen <lars@k7c.dk>
2020-10-16 16:24:25 +0200

    testing auth/me

--- src/app/auth/services/me.service.ts
+++ src/app/auth/services/me.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class MeService {
-  private readonly url = 'AuthMe';
+  private readonly url = '.auth/me';
 
   constructor(private httpClient: HttpClient) {}
 
