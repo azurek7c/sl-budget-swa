467640dc645555fbca17722ddd7fc575d47395a7
Lars <lal@jyskebank.dk>
2020-10-16 22:18:39 +0200

    fixed prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 
-  baseUrl: 'https://ambitious-sand-0a76bec03.azurestaticapps.net/.auth/me',
+  authMeUrl: 'https://ambitious-sand-0a76bec03.azurestaticapps.net/.auth/me',
 };
