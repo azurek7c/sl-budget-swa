8597108b840d3b315c1410ec59bd5773b87867c5
Lars Larsen <lars@k7c.dk>
2020-10-17 09:28:32 +0200

    fixed auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'poster',
     loadChildren: () =>
       import('./poster/poster.module').then((m) => m.PosterModule),
+    canActivate: [AuthGuard],
   },
   {
     path: '',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'sl-budget-swa';
 
-  isAuth: Observable<boolean>;
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.isAuth = this.authService.isAuthenticated();
-  }
+  constructor() {}
 }
